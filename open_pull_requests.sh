25be7d1b24e8ab963ec737a120ed3e0ef576b43d
Harm Aarts <harmaarts@gmail.com>
2017-02-10 09:39:52 +0100

    token: dumb ass me replaced only 1 occurance.
    Github was so nice to mention that to me. Impressed.

--- open_pull_requests.sh
+++ open_pull_requests.sh
@@ -5,7 +5,7 @@ repos=$(curl -s -H "Authorization: token $GH_TOKEN" https://api.github.com/orgs/
 while read -r repo
 do
   trimmed=$(echo $repo | sed 's/{\/number}//')
-  result=$(curl -s -H 'Authorization: token d8ad8372bf215c26cb97365c63206ef01a5a909a' $trimmed | jq ".[] | {title, html_url, created_at}")
+  result=$(curl -s -H "Authorization: token $GH_TOKEN" $trimmed | jq ".[] | {title, html_url, created_at}")
   if [ -n "$result" ]; then
     echo $trimmed | sed 's/https:\/\/api.github.com\/repos\/Bottom-Line-Software//' | sed 's/pulls//'
     echo $result | jq .
